fefbedb82eb1d45813d35455103565e6a2494a41
Conner Henry <connerhenry@conners-macbook-pro.local>
2018-11-14 10:43:33 -0500

    Initial Commit

---
+++ UID-A3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UID-A3
+//
+//  Created by Conner Henry on 2018-11-14.
+//  Copyright © 2018 Conner Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UID-A3Tests/UID_A3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  UID_A3Tests.swift
+//  UID-A3Tests
+//
+//  Created by Conner Henry on 2018-11-14.
+//  Copyright © 2018 Conner Henry. All rights reserved.
+//
+
+import XCTest
+@testable import UID_A3
+
+class UID_A3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
